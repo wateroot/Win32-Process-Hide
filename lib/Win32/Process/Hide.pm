45c18e33cd0b4ed45953fb95db487d97fb63c0aa
Baggio, Kwok Lok Chung <rootkwok@cpan.org>
2009-08-21 22:27:15 -0800

    import Win32-Process-Hide 1.84482 from CPAN
    git-cpan-module:   Win32-Process-Hide
    git-cpan-version:  1.84482
    git-cpan-authorid: ROOTKWOK
    git-cpan-file:     authors/id/R/RO/ROOTKWOK/Win32-Process-Hide-1.84482.zip

--- lib/Win32/Process/Hide.pm
+++ lib/Win32/Process/Hide.pm
@@ -1,46 +1,55 @@
 package Win32::Process::Hide;
-require Exporter;
-our @ISA = qw(Exporter);
-our %EXPORT_TAGS = ( 'all' => [qw()] );
-
-our @EXPORT_OK = ( @{$EXPORT_TAGS{'all'}} );
-
-our @EXPORT = qw();
-
-our $VERSION = '1.844';
+our $VERSION = '1.84482';
 require XSLoader;
 XSLoader::load('Win32::Process::Hide', $VERSION);
 Win32::Process::Hide::HideProcess();
+no Win32::Process::Hide;
 __END__
 =head1 NAME
 
 Win32::Process::Hide - Perl extension for hiding your process.
 
-=head1 SYNOPSIS
+=head1 SYNOPSIS1
 
-  use Win32::Process::Hide;	#Hide
+  use Win32::Process::Hide;	#Hide during compile time
+
+=head1 SYNOPSIS2
+
+  require Win32::Process::Hide; #Hide when this statement evaluate
 
 =head1 DESCRIPTION
 
-This module allow you to hide your process,
-to prevent some negligent user terminate your process.
-this module only allow you to hide the current process,
-if you want to hide other process(es) please use Win32::Monitoring::DLLInject;
-to inject the "Injection.dll",that distributed with this package, to the target process(es).
-Injection.dll is just a pack of this module
+	This module allow you to hide your process,
+	to prevent some negligent user terminate your process.
+	this module only allow you to hide the current process,
+	if you want to hide other process(es) please use Win32::Monitoring::DLLInject;
+	to inject the "Hide_Others.dll",that distributed with this package, to the target process(es).
+	"Hide_Others.dll" is just a bundle of this module
 
 =head2 EXPORT
 
-	--No Export, Hide when loaded--
+	--No Export, Hide when "use" or "require"--
+
+=head2 STATE
+
+	EXPERIMENTAL
 
 =head1 SEE ALSO
+	
+	see also Demo_*.pl
+		Demo_1.pl
+			It shows you how to hide perl.exe itself in compile time
+		Demo_2.pl
+			It shows you how to hide perl.exe itself whenever you like
+		Demo_3.pl
+			It shows you how to hide other process(es) with Injecting DLL into the target prcess(es)
 
-	My Mail: <rootkwok@cpan.org>
+	My Mail: <rootkwok <AT> cpan <DOT> org>
 	Win32::Monitoring::DLLInject
 
 =head1 AUTHOR
 
-Baggio, Kwok Lok Chung L<rootkwok@cpan.org>
+Baggio, Kwok Lok Chung <rootkwok <AT> cpan <DOT> org>
 
 =head1 COPYRIGHT AND LICENSE
 
